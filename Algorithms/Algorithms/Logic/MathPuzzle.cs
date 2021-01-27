45ac2affa20a8da10d5d872aff80a10d73f84480
Kate K <kate.kosolapov@gmail.com>
2021-01-27 15:52:09 -0800

    added isPrime method

---
+++ Algorithms/Algorithms/Logic/MathPuzzle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Algorithms.Algorithms.Logic
+{
+    public class MathPuzzle
+    {
+        public bool IsPrime(int x)
+        {
+            if (x < 2)
+            {
+                return false;
+            }                
+            int sqrt = (int)Math.Sqrt(x);
+            for (var i=2; i<sqrt; i++)
+            {
+                if (x % i == 0)
+                { 
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
