81549ef087fd4d33831853a57d80664f1122f270
Kate K <kate.kosolapov@gmail.com>
2021-02-02 15:54:38 -0800

    added trie creation to practice app

--- PracticeApp/Program.cs
+++ PracticeApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Algorithms.DataStructures.Tree;
 using PracticeApp.Algorithms.Tree;
 
 namespace PracticeApp
@@ -9,11 +10,30 @@ namespace PracticeApp
         {
             Console.WriteLine("Algorithms Examples");
 
-            TreeTraversalExample treeTraversalExample = new TreeTraversalExample();
-            treeTraversalExample.InOrderTraversalTest();
+            //TreeTraversalExample treeTraversalExample = new TreeTraversalExample();
+            //treeTraversalExample.InOrderTraversalTest();
 
-            BTBuilderExample btExample = new BTBuilderExample();
-            btExample.MinimalTreeExample();
+            //BTBuilderExample btExample = new BTBuilderExample();
+            //btExample.MinimalTreeExample();
+
+            TrieCreator trieCreator = new TrieCreator();
+            string[] words = new string[]
+            {
+                "cat",
+                "cant",
+                "cannot",
+                "dog"
+            };
+            Trie firstTree = trieCreator.CreateTrie(words);
+
+            Console.WriteLine(firstTree.Search("c"));
+            Console.WriteLine(firstTree.Search("ca"));
+            Console.WriteLine(firstTree.Search("d"));
+            Console.WriteLine(firstTree.Search("cat"));
+            Console.WriteLine(firstTree.Search("cant"));
+            Console.WriteLine(firstTree.Search("canto"));
+            Console.WriteLine(firstTree.Search("ocat"));
+
         }
     }
 }
