72916479b16f9cff090091374beee9dc501d4863
Kate K <kate.kosolapov@gmail.com>
2021-03-08 23:47:03 -0800

    added recursion problems

---
+++ Algorithms/Algorithms/Recursion/RecursionTool.cs
@@ -0,0 +1,33 @@
+﻿namespace Algorithms.Algorithms.Recursion
+{
+    public class RecursionTool
+    {
+        public int Power(int n, int p)
+        {
+            int result = 1;
+            for(var i=0; i<p; i++)
+            {
+                result *= n;
+            }
+            return result;
+        }
+
+        public int Power2(int n, int p)
+        {
+            if (p == 0)
+                return 1;
+            else
+                return n * Power2(n, p-1);           
+        }
+
+        // 5! = 5 * 4 * 3  *2 * 1 = 120
+        // 0! = 1
+        public int Factorial(int x)
+        {
+            if (x == 0)
+                return 1;
+            else
+                return x * Factorial(x - 1);
+        }
+    }
+}
---
+++ AlgorithmsTest/Algorithms/Recursion/RecursionTest.cs
@@ -0,0 +1,36 @@
+﻿using Algorithms.Algorithms.Recursion;
+using Xunit;
+
+namespace AlgorithmsTest.Algorithms.Recursion
+{
+    public class RecursionTest
+    {
+        [Fact]
+        public void PowerTest()
+        {
+
+            RecursionTool recursionTool = new RecursionTool();
+
+            Assert.Equal(4, recursionTool.Power(2, 2));
+            Assert.Equal(8, recursionTool.Power(2, 3));
+            Assert.Equal(25, recursionTool.Power(5, 2));
+            Assert.Equal(36, recursionTool.Power(6, 2));
+            Assert.Equal(1, recursionTool.Power(7, 0));
+
+            Assert.Equal(4, recursionTool.Power2(2, 2));
+            Assert.Equal(8, recursionTool.Power2(2, 3));
+            Assert.Equal(25, recursionTool.Power2(5, 2));
+            Assert.Equal(36, recursionTool.Power2(6, 2));
+            Assert.Equal(1, recursionTool.Power2(7, 0));
+        }
+
+        [Fact]
+        public void FactorialTest()
+        {
+            RecursionTool recursionTool = new RecursionTool();
+
+            Assert.Equal(120, recursionTool.Factorial(5));
+            Assert.Equal(720, recursionTool.Factorial(6));
+        }
+    }
+}
